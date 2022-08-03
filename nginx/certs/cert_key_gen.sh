68b11770bb51c3d2a0228fb1105afcbb0e3b64de
Ali DALHOUSS <ali.dalahouss@gmail.com>
2022-08-03 18:31:13 +0200

    Refactor, clean and improve the code & structure.

--- nginx/certs/cert_key_gen.sh
+++ nginx/certs/cert_key_gen.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #check if there is no existing certificate
-if [ ! -f /etc/nginx/ssl/laravel.test.crt ]; then
+if [ ! -f /etc/nginx/ssl/laravel.test.crt ] 
+then
     # Create a self-signed key and certificate pair with OpenSSL.
     openssl genrsa -out "/etc/nginx/ssl/laravel.test.key" 4096
     openssl req -new -key "/etc/nginx/ssl/laravel.test.key" -out "/etc/nginx/ssl/laravel.test.csr" -config "/etc/nginx/ssl/ca_laravel_docky.cnf"
